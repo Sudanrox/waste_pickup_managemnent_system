3c5a36f5cd47e78cd2e50ccb1d428d86fe4cc79e
Sudan Khatri <sudankhatri@Mac.lan>
2025-10-26 23:01:28 -0400

    Added frontend and backend folders with starter files

---
+++ backend/server.js
@@ -0,0 +1,5 @@
+echo "import express from 'express';
+const app = express();
+app.get('/', (req, res) => res.send('KWM API Running'));
+app.listen(5000, () => console.log('Server running on port 5000'));
+
